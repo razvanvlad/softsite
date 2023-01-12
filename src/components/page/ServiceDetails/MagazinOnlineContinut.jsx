3c6c2dbc9a6b5e150a8047c5f88f2eca6f18427a
razvanvlad <razvanvladpop@gmail.com>
2023-01-12 20:07:59 +0200

    Content and images fix

--- src/components/page/ServiceDetails/MagazinOnlineContinut.jsx
+++ src/components/page/ServiceDetails/MagazinOnlineContinut.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProceduraServiciu from "./ProceduraServiciu";
+import ListaServiciiMeniuDreapta from "./ListaServiciiMeniuDreapta";
 
 function MagazinOnlineContinut() {
   const scrollTop =()=>window.scrollTo({top:0,behavior:"smooth"})
@@ -26,65 +27,9 @@ function MagazinOnlineContinut() {
                 </p>
               </div>
             </div>
-            <div className="col-lg-4">
-              <div className="sidebar-widget">
-                <h4>Category</h4>
-                <ul className="category">
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      Web Design
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      Apps Development
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      Software Development
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      Motion Graphics
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      UI/UX Design
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={`${process.env.PUBLIC_URL}/project`} onClick={scrollTop}>
-                      Graphic Design
-                      <i className="bi bi-arrow-right" />
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="sidebar-banner">
-                <img src={process.env.PUBLIC_URL + '/img/widget-banner-bg.jpg'} alt="Banner images" />
-                <div className="banner-inner">
-                  <h3>
-                    Any Project <span>Call Now.</span>
-                    <img
-                      className="angle"
-                      src={process.env.PUBLIC_URL + "/img/arrow-angle.png"}
-                      alt=""
-                    />
-                  </h3>
-                  <a href="tel:11231231234">+1-123-123-1234</a>
-                </div>
-              </div>
-            </div>
-          </div>
+<ListaServiciiMeniuDreapta />
           <ProceduraServiciu />
+          </div>
         </div>
       </div>
     </>

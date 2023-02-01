4d8ec60c3d1ca1301088fa387a54ff5e52a6e2ec
razvanvlad <razvanvladpop@gmail.com>
2023-02-01 10:47:29 +0200

    Added button on hero area with lottie animation

--- src/components/page/home/HeroArea.jsx
+++ src/components/page/home/HeroArea.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LottieBanner from "../../common/lottieBanner";
+import LottieHeaderAction from '../../common/LottieHeaderAction';
 
 function HeroArea() {
     const scrollTop =()=>window.scrollTo({top:0,behavior:"smooth"})
@@ -19,8 +20,27 @@ function HeroArea() {
                   <h1>SOFT-URI &amp; SITE-URI<span>SoftSite.ro</span></h1>
                   <p>Oferim o gamă completă de servicii IT. Vă putem dezvolta website-ul de prezentare și vă putem promova afacerea. Lăsați-ne să ne ocupăm de digitalizarea afacerii tale, pentru a le oferi clienților tăi o experiență cât mai plăcută în vederea interacțiunii cu brandul tău.</p>
 
-                 
                   <div className="buttons">
+                  <div className="cmn-btn-layout-two">
+                      <div className="line-1" />
+                      <div className="line-2" />
+                     
+                      <Link to={`${process.env.PUBLIC_URL}/proiecte`} onClick={scrollTop}> 
+                        <div className="action-container" id="lottie-action-container">
+                          <div id="lottie-action">
+                            <LottieHeaderAction />
+                          </div>
+                          <div id="lottie-action-text">
+                            Ofertă Pachetul Digital 
+                            </div>                           
+                        </div>     
+                      </Link>
+                    </div>
+                  </div>
+                 
+
+                  
+                  {/* <div className="buttons">
                     <div className="cmn-btn">
                       <div className="line-1" />
                       <div className="line-2" />
@@ -40,7 +60,7 @@ function HeroArea() {
                       <div className="line-2" />
                       <Link to={`${process.env.PUBLIC_URL}/contact`} onClick={scrollTop}>Cerere ofertă</Link>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>           
